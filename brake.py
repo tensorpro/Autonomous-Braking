282759013bbb5ac6346fb25abdd28ac53e10ffc5
Akshay Mishra <lck.akshay@gmail.com>
2017-05-02 01:01:34 -0500

    Fine tuned

--- brake.py
+++ brake.py
@@ -2,6 +2,12 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+m = .7
+b = -.1
+bl = -.1
+br = .1
+sh = .13
+
 def show_ped(image, bb):
     im = np.zeros(image.shape[:2])
     [ymin, xmin, ymax, xmax] = bb
@@ -15,13 +21,13 @@ def in_region(x,y, m=0, b=0, above=True):
 def y_intercept(x,y,m):
     return y-m*x
 
-def upper_left(bb, m=m, b=b):
+def upper_left(bb, m=m, b=bl):
     [ymin, xmin, ymax, xmax] = bb
     x = (xmin+xmax)/2
     y = (ymin+ymax)/2
     return in_region(x,y,m,b, above=False)
 
-def upper_right(bb, m=m,b=b):
+def upper_right(bb, m=m,b=br):
     [ymin, xmin, ymax, xmax] = bb
     yh = .2
     x = (xmin+xmax)/2
@@ -34,17 +40,21 @@ def brakezone(bb):
     should_brake = ([c(bb) for c in calls])
     return all(should_brake)
 
-def safezone(bb):
+def safezone(bb, sh=sh):
     [ymin, xmin, ymax, xmax] = bb
     x = (xmin+xmax)/2
     y = (ymin+ymax)/2
-    return in_region(x,y, b=.3)
+    return in_region(x,y, b=sh)
 
 def in_trajectory(bb):
     return brakezone(bb) and not safezone(bb)
 
 def show_brakezone(size=[480,480], width=20, height=40, brake_fn=brakezone,
-                   safe_fn=safezone):
+                   safe_fn=safezone, img=None):
+    
+    if img is not None:
+        out = img.copy()
+        size = img.shape[:2]
     img_h, img_w = size
     zone = np.zeros(size)
     dx = width/2
@@ -55,7 +65,17 @@ def show_brakezone(size=[480,480], width=20, height=40, brake_fn=brakezone,
             x = x_/img_w
             bb = [y-dy,x-dx,y+dy,x+dx]
             info = [(15, None, bb)]
-            zone[y_,x_]=int(brake_fn(bb) and not safe_fn(bb))
-    plt.imshow(zone)
-    plt.savefig("killzone")
+            brake = int(brake_fn(bb) and not safe_fn(bb))
+            zone[y_,x_]=brake
+            if img is not None and brake:
+                out[y_,x_][1]+=40
+                # img[0] = max(img[0], 254)
+                
+    
+    # plt.imshow(zone)
+    # plt.savefig("killzone")
+    # plt.show()
+    # plt.close()
+    plt.imshow(out)
+    plt.savefig("mask")
     plt.show()
