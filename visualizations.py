e4edb083e1f6a5866c11c0eac2929ad03b7afbaa
Akshay Mishra <lck.akshay@gmail.com>
2017-05-02 02:09:02 -0500

    Adding visualizations

---
+++ visualizations.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+def show_bboxes(img, result, figsize=(10,10), linewidth=1.5):
+    fig = plt.figure(figsize=figsize)
+    plt.imshow(img)
+    width, height, _ = img.shape
+
+    for r in result:
+        ymin = r['topleft']['y']
+        xmin = r['topleft']['x']
+        ymax = r['bottomright']['y']
+        xmax = r['bottomright']['x']
+        bb = [ymin, xmin, ymax, xmax]
+        lab = r['label']
+        conf = r['confidence']
+
+        if lab not in colors.keys():
+            colors[lab] = (random.random(), random.random(), random.random())
+        rect = plt.Rectangle((xmin, ymin), xmax - xmin,
+                             ymax - ymin, fill=False,
+                             edgecolor=colors[lab],
+                             linewidth=linewidth)
+        plt.gca().add_patch(rect)
+        plt.gca().text(xmin, ymin - 2,
+                       '{:s} | {:.3f}'.format(lab, conf),
+                       bbox=dict(facecolor=colors[lab], alpha=0.5),
+                       fontsize=12, color='white')
